a08b2e8b679e1ff0661f3865874cace8b55c3178
Astral-Lab <87715775+Astral-Lab@users.noreply.github.com>
2022-10-25 19:03:01 +0100

    Add files via upload

--- js/location.js
+++ js/location.js
@@ -4,3 +4,7 @@ import { Navigation } from './modules/navigation.mjs';
 const btns = Navigation.getNavButtons();
 const navigation = new Navigation(btns, URLChain.GetLocalChain());
 navigation.addClickListeners();
+
+// Don't ask
+const iframe = document.querySelector('iframe');
+iframe.src = iframe.src;
